90c9f0ff1a10bd491104779a0192e77b09e29caa
Lucas Muniz <muniz@hotmail.com>
2023-09-22 15:54:34 -0300

    Atualizei o arquivo script.js
    Criei a função criaElemento que recebe 2 parâmetros (nome, qtde)

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ const form = document.getElementById('novoItem'); // "pega" o elemento
 form.addEventListener('submit', evento => {
   // evento é o submit (os dados buscados do formulário)
   evento.preventDefault(); // previne o comportamento padrão do formulário (enviar os dados para a própria página)
+
+  criaElemento(
+    evento.target.elements['nome'].value,
+    evento.target.elements['qtde'].value
+  );
 });
+
+function criaElemento(nome, qtde) {
+  console.log(nome);
+  console.log(qtde);
+}
